7a3ece6e1583dd4151c4bebee2b60e258c145c87
stephanie <stephaniechen0630@gmail.com>
2019-11-30 11:45:18 -0800

    base tableView work

--- mentr/mentr/Controllers/MentorListViewController.swift
+++ mentr/mentr/Controllers/MentorListViewController.swift
@@ -6,4 +6,52 @@
 //  Copyright © 2019 Wren Liang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class MentorListViewController: UIViewController {
+    
+    var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let newView = MentorListView()
+        tableView = newView.tableView
+        self.view = newView
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "Pantry"
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        //tableView.register(MentorListTableViewCell.self, forCellReuseIdentifier: "MentorCell")
+        tableView.separatorStyle = .none
+    }
+}
+
+extension MentorListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MentorCell", for: indexPath)
+        
+        guard let pantryCell = cell as? MentorListTableViewCell else {
+            return cell
+        }
+
+        
+        return pantryCell
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       //TODO: PUSH SHIT
+    }
+    
+}
--- mentr/mentr/Views/MentorListView.swift
+++ mentr/mentr/Views/MentorListView.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class MentorListView: UIView {
     
+    lazy var tableView: UITableView = {
+        var table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        
+        return table
+    }()
+    
     override class var requiresConstraintBasedLayout: Bool {
       return true
     }
@@ -23,10 +30,17 @@ class MentorListView: UIView {
     }
     func layoutView() {
         backgroundColor = .white
+        addSubview(tableView)
+        //addSubview(segmentControl)
 
         setupConstraints()
+
     }
     private func setupConstraints() {
+        tableView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: rightAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
     }
     
     
