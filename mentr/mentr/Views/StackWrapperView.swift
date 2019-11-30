88edf1fb672cbaf40c3c4d8f59a11639ce7256f1
Wren Liang <wren.liang@gmail.com>
2019-11-30 13:19:15 -0800

    Swipe work

--- mentr/mentr/Views/StackWrapperView.swift
+++ mentr/mentr/Views/StackWrapperView.swift
@@ -8,22 +8,61 @@
 
 import UIKit
 class StackWrapperView: UIView {
+    var numCardsToShow: Int = 0
+    var remainingCards: Int = 0
+    var numVisibleCards: Int = 3
+    
+    var swipeDataSource: SwipeCardDataSource? {
+        didSet {
+            reloadData()
+         }
+    }
+    
+    private func reloadData() {
+        guard let swipeDataSource = swipeDataSource else {return}
+        numCardsToShow = swipeDataSource.numberOfCardsToShow()
+        remainingCards = numCardsToShow
+        
+        for i in 0...min(numCardsToShow, numVisibleCards) {
+            
+        }
+    }
+    
+    private func addCardView(card: CardView, index: Int) {
+        card.delegate = self
+        
+        
+    }
+    
     override class var requiresConstraintBasedLayout: Bool {
       return true
     }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         layoutView()
     }
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    
     func layoutView() {
-        backgroundColor = .white
+        backgroundColor = .blue
 
         setupConstraints()
     }
     private func setupConstraints() {
     }
+}
+
+
+extension StackWrapperView: SwipeCardsDelegate {
+    func swipeDidEnd(view: CardView) {
+        <#code#>
+    }
+    
+    
 }
