2cd80556a27359451c4e1af17c5aa86e689a5320
Connor Fong <connor_fong@hotmail.com>
2019-11-30 12:24:15 -0800

    server: batched gets

--- server/src/routers/user.js
+++ server/src/routers/user.js
@@ -2,7 +2,32 @@ const mentors = require('../data/mentors');
 const express = require('express');
 const router = express.Router();
 
-router.get('/user/batch/:count/:index', async (req, res) => {});
+router.get('/user/batch/:index/:count', async (req, res) => {
+    const count = req.params.count;
+    const index = req.params.index;
+
+    if (count <= 0) {
+        res.status(400).send({
+            error: 'Invalid count',
+        });
+        return;
+    }
+
+    if (index > mentors.length - 1 || index < 0) {
+        res.status(400).send({
+            error: 'Invalid starting index',
+        });
+        return;
+    }
+
+    let endIndex = index + count + 1;
+
+    if (endIndex > mentors.length) {
+        endIndex = mentors.length;
+    }
+
+    res.send(mentors.slice(index, endIndex));
+});
 
 router.get('/user/:id', async (req, res) => {
     const id = req.params.id;
@@ -11,6 +36,7 @@ router.get('/user/:id', async (req, res) => {
         res.status(400).send({
             error: 'Invalid user ID',
         });
+        return;
     }
 
     // TODO: secure sensitive information (when added to schema)
