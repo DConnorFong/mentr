f215d8aba6e13de92d94c97520d420b1dce1ead3
Wren Liang <wren.liang@gmail.com>
2019-11-30 12:01:28 -0800

    Update Person.swift

--- mentr/mentr/Models/Person.swift
+++ mentr/mentr/Models/Person.swift
@@ -6,8 +6,44 @@
 //  Copyright © 2019 Wren Liang. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct Person {
+    var name: String
+    var animoji: UIImage?
+    var major: majors
+    var university: String
+    var skills: [String]
+    var bio: String
+    
+    //MARK: - Contact
+    var facebook: String
+    var email: String
+    var phoneNum: String
+    var wechat: String
+}
+
+
+enum majors {
+    //MARK: - Engineering
+    case cpen
+    case elec
+    case civl
+    case mech
+    
+    //MARK: - Science
+    case cpsc
+    case biol
+    case chem
+    
+    
+    //MARK: - Arts
+    case film
+    case music
+    case visual
+    case history
+    
+    //MARK: - Kinesiology
+    case kines
     
 }
