b6fdccc0398f6aa3160d4b16a6caeea6e4b36926
stephanie <stephaniechen0630@gmail.com>
2019-11-30 17:33:56 -0800

    modal done

--- mentr/mentr/Controllers/SettingsViewController.swift
+++ mentr/mentr/Controllers/SettingsViewController.swift
@@ -9,11 +9,33 @@
 import UIKit
 
 class SettingsViewController: UIViewController {
-    
+    var checkedArray: [UIButton] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         let newView = SettingsView()
-        
+        checkedArray.append(newView.checkBox)
+        checkedArray.append(newView.checkBox2)
+        checkedArray.append(newView.checkBox3)
+        newView.checkBox.addTarget(self, action: #selector(checkMarkTapped), for: .touchUpInside)
+        newView.checkBox2.addTarget(self, action: #selector(checkMarkTapped), for: .touchUpInside)
+        newView.checkBox3.addTarget(self, action: #selector(checkMarkTapped), for: .touchUpInside)
+
         self.view = newView
+        
+        let testModal = ModalPopUpViewController()
+        
+        self.present(testModal, animated: true) {
+            print("presented")
+        }
+    }
+    
+    @objc func checkMarkTapped(_ sender: UIButton) {
+             for x in 0...2 {
+                 checkedArray[x].isSelected = false
+             }
+            UIView.animate(withDuration: 0.5, delay: 0.1, options: .curveLinear, animations: {
+                sender.isSelected = !sender.isSelected
+                sender.transform = .identity
+            }, completion: nil)
     }
 }
