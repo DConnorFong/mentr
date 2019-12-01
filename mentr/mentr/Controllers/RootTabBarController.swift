4bd1e6d1d0f3e95a26770c5c3b84355ba609b7ab
Alan Yan <yanalan7@gmail.com>
2019-11-30 19:39:37 -0800

    resizing

--- mentr/mentr/Controllers/RootTabBarController.swift
+++ mentr/mentr/Controllers/RootTabBarController.swift
@@ -18,7 +18,7 @@ class RootTabBarController: UITabBarController {
     var apiButton: UIButton = {
         let button = UIButton()
         button.backgroundColor = .gray
-        button.layer.cornerRadius = 40
+        button.layer.cornerRadius = 30
         button.addTarget(self, action: #selector(testAPICall), for: .touchUpInside)
         button.setImage(UIImage(named: "refresh"), for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
@@ -74,9 +74,9 @@ class RootTabBarController: UITabBarController {
     //use to populate colors or person
     private func constraintLayout() {
         apiButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        apiButton.heightAnchor.constraint(equalToConstant: 80).isActive = true
-        apiButton.widthAnchor.constraint(equalToConstant: 80).isActive = true
-        apiButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -110).isActive = true
+        apiButton.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        apiButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
+        apiButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100).isActive = true
 
     }
     
